f4cd52faa6f6cce45c8b9deb6c5584dfe447ee69
Balázs Sipos <thesiposthe@gmail.com>
2021-03-13 18:03:28 +0100

    Added the 9 basic cards

--- lib/pages/satistics_page.dart
+++ lib/pages/satistics_page.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
 
 class StatisticsPage extends StatelessWidget {
+  final List<String> statList = [
+    "Username ItaliX",
+    "Kills: 4215",
+    "Deaths: 2860",
+    "Assists: 667",
+    "K/D: 1.1474",
+    "Matches: 180",
+    "Won: 90",
+    "Lost: 90",
+    "Win %: 50%",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [Expanded(child: buildStatisticsCard(context, 0))],
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(statList.length - 1,
+                  (index) => buildStatisticsCard(context, index + 1)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildStatisticsCard(BuildContext context, int index) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Text(statList[index]),
+          ],
+        ),
+      ),
+      margin: EdgeInsets.all(10.0),
+      color: Colors.lightBlue,
+    );
   }
 }
